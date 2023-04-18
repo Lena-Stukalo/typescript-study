2302b82e31ff5a1070f6e68afc9ec0d60d01735e
Olena Stukalo <olena.stukalo@plus4u.net>
2023-04-18 20:49:58 +0300

    leson2

--- app.ts
+++ app.ts
@@ -1,10 +1,86 @@
-const button = document.querySelector("button")!;
-const input1 = document.getElementById("num1")! as HTMLInputElement;
-const input2 = document.getElementById("num2")! as HTMLInputElement;
+//task 1
+let age: number;
+let names: string;
+let toggle: boolean;
+let empty: null;
+let notInitialize: undefined;
+let callback: (a: number) => number;
 
-function add(num1: number, num2: number) {
+age = 50;
+names = "Max";
+toggle = true;
+empty = null;
+notInitialize = undefined;
+callback = (a) => {
+  return 100 + a;
+};
+
+// task 2
+let anything: any;
+
+anything = -20;
+anything = "Text";
+anything = {};
+
+// task 3
+let some: unknown;
+some = "Text";
+
+let str: string;
+if (typeof some === "string") {
+  str = some;
+}
+
+//task 4
+let person: [string, number];
+person = ["Max", 21];
+
+//task 5
+enum Load {
+  LOADING,
+  READY,
+}
+// task 6
+let union: number | string;
+// task 7
+let literal: "enable" | "disable";
+//task 8
+function showMessage(message: string): void {
+  console.log(message);
+}
+
+function calc(num1: number, num2: number): number {
   return num1 + num2;
 }
-button.addEventListener("click", () => {
-  console.log(add(+input1.value, +input2.value));
-});
+
+function customError(): never {
+  throw new Error("Error");
+}
+// task 9
+type DataType = {
+  title: string;
+  likes: number;
+  accounts: string[];
+  status: "open" | "close";
+  details?: {
+    createAt: Date;
+    updateAt: Date;
+  };
+};
+const page1 = {
+  title: "The awesome page",
+  likes: 100,
+  accounts: ["Max", "Anton", "Nikita"],
+  status: "open",
+  details: {
+    createAt: "2021-01-01",
+    updateAt: "2021-05-01",
+  },
+};
+
+const page2 = {
+  title: "Python or Js",
+  likes: 5,
+  accounts: ["Alex"],
+  status: "close",
+};
